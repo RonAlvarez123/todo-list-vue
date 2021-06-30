6207fb2e814548eba5ce7b2dfa673fdace16e5ce
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-06-30 16:08:39 +0800

    added TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li v-for="lists in List" :key="lists">
+    <p ref="listItem">{{ lists }}</p>
+    <button @click="remove">Remove</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["List"],
+  methods: {
+    remove(e) {
+      this.$emit("removeItem", e.target.parentElement.childNodes[0].innerText);
+    },
+  },
+};
+</script>
+
+<style>
+</style>

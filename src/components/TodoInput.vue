89c4c078b06d478b5c761d31206faf1afa766955
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-06-30 16:08:19 +0800

    added TodoInput component

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="text" ref="Input" v-model="inputValue" />
+  <button @click="add">Add Item</button>
+</template>
+
+<script>
+export default {
+  name: "TodoInput",
+  data() {
+    return {
+      itemValue: undefined,
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("addItem", this.inputValue);
+      this.inputValue = undefined;
+      this.$refs.Input.value = "";
+      this.$refs.Input.focus();
+    },
+  },
+};
+</script>
+
+<style>
+li {
+  display: grid;
+  grid-template-columns: auto 80px;
+  list-style-type: none;
+}
+</style>
